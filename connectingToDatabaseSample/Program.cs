9afd78fc64b1e2aef49e9ea58c109681909afa00
Chuka Ogbodo <chukaogbodo7@gmail.com>
2023-02-15 09:30:09 +0100

    some updates, still needs improvements

--- connectingToDatabaseSample/Program.cs
+++ connectingToDatabaseSample/Program.cs
@@ -11,7 +11,7 @@ namespace connectingToDatabaseSample
             {
                 String ConnectionString;
                 SqlConnection connection = new SqlConnection(@"Data Source =.; Initial Catalog = BzAtmApp; Integrated Security = True");
-                ConnectionString = "CREATE DATABASE NewBtmApp";
+                ConnectionString = "CREATE DATABASE BzAtmmApp";
 
                 SqlCommand myCommand = new SqlCommand(ConnectionString, connection);
                 try
@@ -34,7 +34,40 @@ namespace connectingToDatabaseSample
 
             }
 
-            createDB();
+            //createDB();
+            void CreateUserTable()
+            {
+                SqlConnection connection = new SqlConnection(@"Data Source=.; Initial Catalog= BzAtmApp; Integrated Security=True");
+                string createQ = "CREATE TABLE Users( id INT UNIQUE IDENTITY(1,1) NOT NULL," +
+                        "userId uniqueidentifier NOT NULL  DEFAULT newid()," +
+                        "name VARCHAR(70) NOT NULL, " +
+                        "cardNumber VARCHAR(15) NOT NULL UNIQUE, " +
+                        "cardPin VARCHAR(4) NOT NULL, " +
+                        "balance DECIMAL(38,2) NOT NULL, " +
+                        "status BIT NOT NULL, " +
+                        "PRIMARY KEY(Id))";
+
+                SqlCommand myCommand = new SqlCommand(createQ, connection);
+                try
+                {
+                    connection.Open();
+                    myCommand.ExecuteNonQuery();
+                    Console.WriteLine("User Table Created Successfully");
+                }
+                catch (System.Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+                finally
+                {
+                    if (connection.State == ConnectionState.Open)
+                    {
+                        connection.Close();
+                    }
+                }
+
+            }
+            CreateUserTable();
         }
     }
 }
