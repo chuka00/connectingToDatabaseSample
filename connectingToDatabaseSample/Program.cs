2aa0561716018b94362ed29a826b0a909fac7a25
Chuka Ogbodo <chukaogbodo7@gmail.com>
2023-02-15 08:47:52 +0100

    some sample code

---
+++ connectingToDatabaseSample/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace connectingToDatabaseSample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            void createDB()
+            {
+                String ConnectionString;
+                SqlConnection connection = new SqlConnection(@"Data Source =.; Initial Catalog = BzAtmApp; Integrated Security = True");
+                ConnectionString = "CREATE DATABASE NewBtmApp";
+
+                SqlCommand myCommand = new SqlCommand(ConnectionString, connection);
+                try
+                {
+                    connection.Open();
+                    myCommand.ExecuteNonQuery();
+                    Console.WriteLine("DataBase is Created Successfully");
+                }
+                catch (System.Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+                finally
+                {
+                    if (connection.State == ConnectionState.Open)
+                    {
+                        connection.Close();
+                    }
+                }
+
+            }
+
+            createDB();
+        }
+    }
+}
